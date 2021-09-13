67e7ab5fc21397ddf440ac5a02b0448c51c4b3a6
Michael <mihail.tchernov@yandex.ru>
2021-09-13 15:59:35 +0300

    Добавлена генерация случайных точек в пределах экстента

--- dataset.py
+++ dataset.py
@@ -16,11 +16,19 @@ class Limit(NamedTuple):
 
 
 class InterpolationParameter(NamedTuple):
+    time_lim: Limit
     x_lim: Limit
     y_lim: Limit
-    time_lim: Limit
     cell_size: float
     buffer_distance: float
+
+    @property
+    def extent_x_size(self) -> float:
+        return self.x_lim.max_val - self.x_lim.min_val
+
+    @property
+    def extent_y_size(self) -> float:
+        return self.y_lim.max_val - self.y_lim.min_val
 
 
 class Dataset:
@@ -31,10 +39,15 @@ class Dataset:
             usecols=[columns.time, columns.x, columns.y, columns.z,
                      columns.density]
         )
+        self.__clear_data = self.get_clear_data()
 
     @property
     def src_data(self) -> np.ndarray:
         return self.__src_data
+
+    @property
+    def clear_data(self):
+        return self.__clear_data
 
     def get_clear_data(self) -> np.ndarray:
         hash_vals = set()
@@ -46,4 +59,22 @@ class Dataset:
                 hash_vals.add(current_hash)
         return clear_array
 
+    def get_selection(self, params: InterpolationParameter) -> np.ndarray:
+        arr = self.clear_data
+        if not arr.shape[0]:
+            return np.array([])
 
+        arr = arr[(arr[ColumnIndex.time] >= params.time_lim.min_val) *
+                  (arr[ColumnIndex.time] < params.time_lim.max_val)]
+
+        arr = arr[(arr[ColumnIndex.x] >= params.x_lim.min_val) *
+                        (arr[ColumnIndex.x] < params.x_lim.max_val)]
+
+        arr = arr[(arr[ColumnIndex.y] >= params.y_lim.min_val) *
+                  (arr[ColumnIndex.y] < params.y_lim.max_val)]
+        return arr
+
+    @staticmethod
+    def get_random_points_count(params: InterpolationParameter):
+        extent_area = params.extent_x_size * params.extent_y_size
+        return int(extent_area / params.cell_size ** 2)
